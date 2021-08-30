572496c47454c1ce12fbd7ec050b36e4ff8a5875
unknown <polozovnikita93@mail.ru>
2021-08-30 18:37:27 +0300

    homework

---
+++ homework_12.7.3.py
@@ -0,0 +1,10 @@
+money = float(input("Введите сумму депозита: "))
+per_cent = {'ТБК': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+my_deposit = []
+my_deposit.append(per_cent['ТБК'] / 100 * money)
+my_deposit.append(per_cent['СКБ'] / 100 * money)
+my_deposit.append(per_cent['ВТБ'] / 100 * money)
+my_deposit.append(per_cent['СБЕР'] / 100 * money)
+print(list(map(round, my_deposit)))
+max_deposit = my_deposit[1]
+print('Максимальная сумма, которую вы можете заработать: ', round(max_deposit))
